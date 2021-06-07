0649274d5c44d2d23a8d2050c756116bdab49b35
+ <git-commit-mcreasmith23@gmail.com>
2021-06-07 17:03:37 -0500

    src: imported reducers

--- src/store.js
+++ src/store.js
@@ -1,1 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleweare = [thunk];
+
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleweare));
+
+export default store;
