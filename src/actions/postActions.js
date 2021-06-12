54a000fd06990d6c2d7999ec4f5a6711edca21f3
+ <git-commit-mcreasmith23@gmail.com>
2021-06-12 13:23:53 -0500

    immorted connect

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -3,8 +3,10 @@ import { FETCH_POSTS, NEW_POST } from '../types';
 export const fetchPosts = () => dispatch => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(posts => dispatch({
+    .then(posts => 
+      dispatch({
       type: FETCH_POSTS,
       posts: posts
-    }));
+    })
+   );
 }
