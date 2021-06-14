c23eb477180cd7a52e89e4ae9fc3363809d9578e
+ <git-commit-mcreasmith23@gmail.com>
2021-06-14 18:09:07 -0500

    src:

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POST } from '../types';
+import { FETCH_POSTS, NEW_POST } from './types';
 
 export const fetchPosts = () => dispatch => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -6,7 +6,7 @@ export const fetchPosts = () => dispatch => {
     .then(posts => 
       dispatch({
       type: FETCH_POSTS,
-      posts: posts
+      payload: posts
     })
    );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { connect }  from 'react-redux';
-
+import { fetchPosts } from '../actions/postActions';
 class Posts extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p> 
@@ -19,5 +23,9 @@ class Posts extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
 
-export default Posts;
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
